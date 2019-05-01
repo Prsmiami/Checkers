89649da169f641e6a1521e478bdbd162179d4876
Patrick Sutton <44308166+Prsmiami@users.noreply.github.com>
2019-05-01 16:25:08 -0400

    Add files via upload

---
+++ CheckersClient.py
@@ -0,0 +1,198 @@
+import socket
+import datetime
+import stddraw
+import sys
+
+def f(x):   #dictionary for what color circle will be drawn over black square
+    return {
+        1: 'WHITE',
+        2: 'RED'
+    }.get(x, 'BLACK')   #default is black, so nothing shows if values[z]=0
+
+def printcheckboard(values): 
+    n = 8
+    stddraw.setXscale(0, n)
+    stddraw.setYscale(0, n)
+    z = 0 ##z is the spot in values string
+    rows = 'ABCDEFGH'
+    cols = '12345678'
+        
+    for i in range(n):
+        for j in range(n): 
+            if ((i + j) % 2) != 0:
+                stddraw.setPenColor(stddraw.BLACK)
+            else:
+                stddraw.setPenColor(stddraw.RED)
+            stddraw.filledSquare(j + .5, i + .5, .5)
+            stddraw.setPenColor(stddraw.YELLOW)
+            location = rows[i] + cols[j]
+            stddraw.text(j+.15, i+.1, location)
+            if((i + j) % 2) == 0:
+                if(values[z] == '1'):           ##player 1 #or values[z] == '3'
+                    stddraw.setPenColor(stddraw.BLUE)
+                    stddraw.filledCircle(j + .5, i + .5, .25)
+                elif(values[z] == '2'):         ##player 2
+                    stddraw.setPenColor(stddraw.WHITE)
+                    stddraw.filledCircle(j + .5, i + .5, .25)
+                elif(values[z] == '3'):         ##player 1 king
+                    stddraw.setPenColor(stddraw.WHITE)
+                    stddraw.filledCircle(j + .5, i + .5, .25)
+                    stddraw.setPenColor(stddraw.BLACK)
+                    stddraw.text(j+.5, i+.5, "K")
+                elif(values[z] == '4'):         ##player 2 king
+                    stddraw.setPenColor(stddraw.WHITE)
+                    stddraw.filledCircle(j + .5, i + .5, .25)
+                    stddraw.setPenColor(stddraw.BLACK)
+                    stddraw.text(j+.5, i+.5, "K")
+                z+=1
+    stddraw.show(500)
+    
+
+def loopRecv(csoc, size):
+    data = bytearray(b" "*size)
+    mv = memoryview(data)
+    while size:
+        rsize = csoc.recv_into(mv,size)
+        mv = mv[rsize:]
+        size -= rsize
+    return data
+
+def inbounds(move):
+    rows="ABCDEFGH"
+    cols="12345678"
+    startindex=0
+    destindex=0
+    m0 = False
+    m1 = False
+    m2 = False
+    m3 = False
+    total = False
+    for i in range(8):
+        if(move[0] == rows[i]):
+            startindex += 8*(i)
+            m0 = True
+        if(move[1] == cols[i]):
+            startindex += (i)
+            m1 = True
+        if(move[2] == rows[i]):
+            destindex += 8*(i)
+            m2 = True
+        if(move[3] == cols[i]):
+            destindex += (i)
+            m3 = True
+    total = m0 and m1 and m2 and m3
+    valid = total
+    return valid
+
+def inputDoubleJump(csoc,oldmove):
+    flag=0
+    while(flag==0):
+        print("You can make another jump with the same piece, what do you request as the new destination(only type the destination)?")
+        move = input()
+        while (len(move) != 2 and abs (ord(oldmove[2]) - ord(move[0])) ==2):
+            print("Please enter a valid 2 character jump move in the form of \n[nextrow][nextcolumn] \nfor example: D2")
+            move = input()
+        #sends a move of valid size to server to check if move is valid
+        print("sending correct sized move to check for validity")
+        fullmove = oldmove[2] + oldmove[3] + move
+        print(fullmove)
+        commsoc.sendall((fullmove).encode("utf-8"));
+        print("move2 sent to server, now wait for server to respond...")
+        isValid = loopRecv(csoc,1).decode()
+        print("server sent back for isValid  ",isValid)
+        if(isValid == 'I'):
+            print("move invalid, terrible job")
+            flag=0
+        elif(isValid == 'V'):
+            print("move valid! good job")
+            flag=1
+        elif(isValid == 'A'):   #you made a jump, maybe go again
+            print("you jumped a piece! good job")
+            flag=1
+
+def inputmove(csoc,board):
+    flag=0
+    while(flag==0):
+        move = input()
+
+        #debug, print list of valid moves
+        #print(validmoves(move,board))
+        
+        while ((len(move) != 4) or not(inbounds(move))):
+            if(len(move) != 4):
+                print("Please enter a valid 4 character move in the form of \n[currentrow][currentcolumn][nextrow][nextcolumn] \nfor example: C1D2")
+            if(not(inbounds(move))):
+                print("Locations must be between rows A and H, and columns 1 and 8")
+            move = input()
+        #sends a move of valid size to server to check if move is valid
+        print("sending correct sized move to check for validity")
+        commsoc.sendall((move).encode("utf-8"));
+        print("move sent to server, now wait for server to respond...")
+        isValid = loopRecv(csoc,1).decode()
+        print("server sent response ",isValid)
+        if(isValid == 'I'):
+            print("move invalid, terrible job")
+            flag=0
+        elif(isValid == 'V'):
+            print("move valid! good job")
+            flag=1
+        elif(isValid == 'A'):
+            print("you're in position to make a double jump!!!")
+            turn = loopRecv(csoc,1).decode()
+            print(turn)
+            board = loopRecv(csoc,32).decode()
+            printcheckboard(board)
+            print("do you want to make another move? yes/no")
+            confirmation = input()
+            if( confirmation == "y" or confirmation == "Y" or confirmation == "yes" or confirmation == "Yes"):
+                
+                inputDoubleJump(csoc,move)
+            flag=1
+        
+
+def Application(csoc):
+    startBoard='11111111111100000000222222222222'   #base start board
+    print(loopRecv(csoc,8).decode())    #print which socket this client is
+    while (True):
+        turn = loopRecv(csoc,1).decode()
+        print(turn)
+        board = loopRecv(csoc,32).decode()
+        print(board)
+        printcheckboard(board)
+        if(turn=="T"):          #if Turn, send 4 char move to server
+            inputmove(csoc,board)
+    
+            
+if __name__ == "__main__":
+    # create the socket
+    #  defaults family=AF_INET, type=SOCK_STREAM, proto=0, filno=None
+    commsoc = socket.socket()
+    
+    # connect to localhost:5000
+    commsoc.connect(("localhost",50000))
+
+    print("Started Application")
+
+    while(True):
+        print("Please type either 'random' for a random opponent or a 6 digit alphanumeric room code for a match with a friend.")
+        roomtype = ""
+        roomtype = input()
+
+        print("You typed: ",roomtype,", is this correct?");
+        confirmation = input();
+        if(len(roomtype) == 6):
+            if( confirmation == "y" or confirmation == "Y" or confirmation == "yes" or confirmation == "Yes"): break
+        else: print("Must enter either 'random' or a 6 digit alphanumeric code")
+
+    if(roomtype == "random" or roomtype == "Random"):
+        commsoc.sendall(("R").encode("utf-8"));
+    else:
+        commsoc.sendall(("C").encode("utf-8"));
+        commsoc.sendall((roomtype).encode("utf-8"));
+
+    
+    # run the application protocol    
+    Application(commsoc)
+    
+    # close the comm socket
+    commsoc.close()
---
+++ CheckersServer.py
@@ -0,0 +1,477 @@
+import selectors
+import socket
+import threading
+import random
+
+def loopRecv(csoc, size):
+    data = bytearray(b" "*size)
+    mv = memoryview(data)
+    while size:
+        rsize = csoc.recv_into(mv,size)
+        mv = mv[rsize:]
+        size -= rsize
+    return data
+
+class waitinguser:
+    prevuser = 0
+    socket = 0
+    roomcode = 0
+    nextuser = 0
+
+def make_waitinguser(var1,var2):
+    user = waitinguser()
+    user.socket = var1
+    user.roomcode = var2
+    return user
+
+def doAccept(thesel, thesock, mask):
+    # accept the connection
+    commsoc, raddr = thesock.accept()
+    print("Connection from:", raddr)
+    # set non-blocking
+    commsoc.setblocking(False)
+    # register
+    thesel.register(commsoc, selectors.EVENT_READ, doRead)
+
+def endgamecheck(board):
+    p1flag = 0
+    p2flag = 0
+    i = 28
+    while(i<32):
+        if(board[i] == 1):
+            p1flag = 1
+        i += 1
+    i = 0
+    while(i<4):
+        if(board[i] == 2):
+            p2flag = 1
+        i += 1
+    if(p1flag == 1): return(1)
+    if(p2flag == 1): return(2)
+    return(0)
+
+def Roomhandler(sock1, sock2, mask):
+
+    sock1.setblocking(True)
+    sock2.setblocking(True)
+    sock1.sendall(("socket 1").encode("utf-8"))
+    sock2.sendall(("socket 2").encode("utf-8"))
+##  turn 1 is for socket 1
+    endgame = 0
+    goagain1 = 0
+    goagain2 = 0
+    turn = 1
+    endgame = 0
+    mult = 0
+    base = 0
+    fromindex = 0
+    toindex = 0
+    difference = 0
+    move = ""
+    board = [1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2,2,2,2]
+    while(endgame == 0):
+        if(turn == 1):
+            if(goagain1 == 0):
+                sock1.sendall(('T').encode("utf-8"))
+                sock2.sendall(('N').encode("utf-8"))
+                for e in board:
+                    sock1.sendall(str(e).encode("utf-8"))
+                    sock2.sendall(str(e).encode("utf-8"))
+                print("about to send board before CLIENT1(one) move")
+            goagain1 = 0
+            move = (loopRecv(sock1,4)).decode()
+            print(move)
+            print("Sent board after CLIENT1(one) move\n")
+#0  #1  #3  #4  #5  #6  #7  #8 this is to check indentation
+            if(move[0] == 'A'):
+                mult = 0
+            elif (move[0] == 'B'):
+                mult = 1
+            elif(move[0] == 'C'):
+                mult = 2
+            elif(move[0] == 'D'):
+                mult = 3
+            elif(move[0] == 'E'):
+                mult = 4
+            elif(move[0] == 'F'):
+                mult = 5
+            elif(move[0] == 'G'):
+                mult = 6
+            elif(move[0] == 'H'):
+                mult = 7
+            if(move[1] == '1'):
+                base = 0
+            elif(move[1] == '2'):
+                base = 0
+            elif(move[1] == '3'):
+                base = 1
+            elif(move[1] == '4'):
+                base = 1
+            elif(move[1] == '5'):
+                base = 2
+            elif(move[1] == '6'):
+                base = 2
+            elif(move[1] == '7'):
+                base = 3
+            elif(move[1] == '8'):
+                base = 3
+            fromindex = base + (mult*4)
+
+            if(move[2] == 'A'):
+                mult = 0
+            elif(move[2] == 'B'):
+                mult = 1
+            elif(move[2] == 'C'):
+                mult = 2
+            elif(move[2] == 'D'):
+                mult = 3
+            elif(move[2] == 'E'):
+                mult = 4
+            elif(move[2] == 'F'):
+                mult = 5
+            elif(move[2] == 'G'):
+                mult = 6
+            elif(move[2] == 'H'):
+                mult = 7
+            if(move[3] == '1'):
+                base = 0
+            elif(move[3] == '2'):
+                base = 0
+            elif(move[3] == '3'):
+                base = 1
+            elif(move[3] == '4'):
+                base = 1
+            elif(move[3] == '5'):
+                base = 2
+            elif(move[3] == '6'):
+                base = 2
+            elif(move[3] == '7'):
+                base = 3
+            elif(move[3] == '8'):
+                base = 3
+            toindex = base + (mult*4)               
+
+
+            if((ord(move[0]) == (ord(move[2])-1)) and move[0] != 'H'):
+                print("MOVE debug ",board[fromindex])
+                if(board[fromindex] == 1):
+                    if(board[toindex] == 0):
+                        board[fromindex] = 0
+                        board[toindex] = 1
+                        sock1.sendall(("V").encode("utf-8"))
+                    else:
+                        print("Please select a space that is not occupied by another piece.")
+                        sock1.sendall(("I").encode("utf-8"))
+                        goagain1=1
+                else:
+                    print("Please select a piece that is yours. Your pieces are on the bottom.")
+                    sock1.sendall(("I").encode("utf-8"))
+                    goagain1=1
+            elif((ord(move[0]) == (ord(move[2])-2)) and (move[0] != 'H') and (move[0] != 'G')):
+                print("JUMP debug ",board[fromindex])
+                print(fromindex,"    ",board[fromindex])
+                print("difference =   ",toindex-fromindex)
+                print("toindex  =   ",toindex)
+                if(board[fromindex] == 1):
+                    difference = toindex-fromindex
+                    print("DEBUG ",difference)  #
+                    if(difference == 9): #UP AND RIGHT
+                        if(board[fromindex+4] == 2):
+                            board[fromindex+4] = 0
+                            board[fromindex] = 0
+                            board[toindex] = 1
+                        if(toindex<24):
+                            if(toindex%4 == 0):
+                                if(board[toindex+9] == 0 and board[toindex+4] == 2):
+                                    sock1.sendall(("A").encode("utf-8"))
+                                    goagain1 = 1
+                            elif(toindex%4 == 3):
+                                print("out of bounds comes up with ",toindex+7)
+                                if(board[toindex+7] == 0 and board[toindex+3] == 2):
+                                    sock1.sendall(("A").encode("utf-8"))
+                                    goagain1 = 1
+                            elif((board[toindex+9] == 0 and board[toindex+4] == 2) or (board[toindex+7] == 0 and board[toindex+3] == 2)):
+                                sock1.sendall(("A").encode("utf-8"))
+                                goagain1 = 1
+                            else: sock1.sendall(("V").encode("utf-8"))
+                        else: sock1.sendall(("V").encode("utf-8"))
+                    elif(difference == 7): #UP AND LEFT
+                        if(board[fromindex+3] == 2):
+                            board[fromindex+3] = 0
+                            board[fromindex] = 0
+                            board[toindex] = 1
+                        if(toindex<24):
+                            if(toindex%4 == 0):
+                                if(board[toindex+9] == 0 and board[toindex+4] == 2):
+                                    sock1.sendall(("A").encode("utf-8"))
+                                    goagain1 = 1
+                            elif(toindex%4 == 3):
+                                if(board[toindex+7] == 0 and board[toindex+3] == 2):
+                                    sock1.sendall(("A").encode("utf-8"))
+                                    goagain1 = 1
+                            elif((board[toindex+9] == 0 and board[toindex+4] == 2) or (board[toindex+7] == 0 and board[toindex+3] == 2)):
+                                sock1.sendall(("A").encode("utf-8"))
+                                goagain1 = 1
+                            else: sock1.sendall(("V").encode("utf-8"))
+                        else: sock1.sendall(("V").encode("utf-8"))
+                    else:
+                        print("Invalid move")
+                        sock1.sendall(("I").encode("utf-8"))
+                        goagain1=1
+                else:
+                    print("Please select a piece that is yours. Your pieces are on the bottom")
+                    sock1.sendall(("I").encode("utf-8"))
+                    goagain1=1
+            print("after move/jump loops")
+            if(goagain1 == 0):
+                turn = 2
+#0  #1  #3  #4  #5  #6  #7  #8 this is to check indentation
+
+
+                
+        #BEGIN TURN CLIENT 2
+        elif(turn == 2):
+            if(goagain2 == 0):
+                sock2.sendall(("T").encode("utf-8"))
+                sock1.sendall(("N").encode("utf-8"))
+                for e in board:
+                    sock1.sendall(str(e).encode("utf-8"))
+                    sock2.sendall(str(e).encode("utf-8"))
+                print("about to send board before CLIENT2 move")
+            move = (loopRecv(sock2,4)).decode()
+            print(move)
+            print("Sent board after CLIENT2(two) move\n")
+            goagain2 = 0
+            if(move[0] == 'A'):
+                mult = 0
+            elif(move[0] == 'B'):
+                mult = 1
+            elif(move[0] == 'C'):
+                mult = 2
+            elif(move[0] == 'D'):
+                mult = 3
+            elif(move[0] == 'E'):
+                mult = 4
+            elif(move[0] == 'F'):
+                mult = 5
+            elif(move[0] == 'G'):
+                mult = 6
+            elif(move[0] == 'H'):
+                mult = 7
+            if(move[1] == '1'):
+                base = 0
+            elif(move[1] == '2'):
+                base = 0
+            elif(move[1] == '3'):
+                base = 1
+            elif(move[1] == '4'):
+                base = 1
+            elif(move[1] == '5'):
+                base = 2
+            elif(move[1] == '6'):
+                base = 2
+            elif(move[1] == '7'):
+                base = 3
+            elif(move[1] == '8'):
+                base = 3
+            fromindex = base + (mult*4)
+
+            if(move[2] == 'A'):
+                mult = 0
+            elif(move[2] == 'B'):
+                mult = 1
+            elif(move[2] == 'C'):
+                mult = 2
+            elif(move[2] == 'D'):
+                mult = 3
+            elif(move[2] == 'E'):
+                mult = 4
+            elif(move[2] == 'F'):
+                mult = 5
+            elif(move[2] == 'G'):
+                mult = 6
+            elif(move[2] == 'H'):
+                mult = 7
+            if(move[3] == '1'):
+                base = 0
+            elif(move[3] == '2'):
+                base = 0
+            elif(move[3] == '3'):
+                base = 1
+            elif(move[3] == '4'):
+                base = 1
+            elif(move[3] == '5'):
+                base = 2
+            elif(move[3] == '6'):
+                base = 2
+            elif(move[3] == '7'):
+                base = 3
+            elif(move[3] == '8'):
+                base = 3
+            toindex = base + (mult*4)
+
+            if((ord(move[0]) == (ord(move[2])+1)) and move[0] != 'A'):
+                if(board[fromindex] == 2):
+                    if(board[toindex] == 0):
+                        board[fromindex] = 0
+                        board[toindex] = 2
+                        sock2.sendall(("V").encode("utf-8"))
+                    else:
+                        print("Please select a space that is not occupied by another piece.")
+                        sock2.sendall(("I").encode("utf-8"))
+                        goagain2=1
+                else:
+                    print("Please select a piece that is yours. Your pieces are on the bottom.")
+                    sock2.sendall(("I").encode("utf-8"))
+                    goagain2=1
+            elif((ord(move[0]) == (ord(move[2])+2)) and (move[0] != 'A') and (move[0] != 'B')):
+                if(board[fromindex] == 2):
+                    difference = toindex-fromindex
+                    if(difference == -9): #DOWN AND LEFT
+                        if(board[fromindex-4] == 1):
+                            board[fromindex-4] = 0
+                            board[fromindex] = 0
+                            board[toindex] = 2
+                        if(toindex>7):
+                            if(toindex%4 == 3):
+                                if(board[toindex-9] == 0 and board[toindex-4] == 1):
+                                    sock2.sendall(("A").encode("utf-8"))
+                                    goagain2 = 1
+                            elif(toindex%4 == 0):
+                                if(board[toindex-7] == 0 and board[toindex-3] == 1):
+                                    sock2.sendall(("A").encode("utf-8"))
+                                    goagain2 = 1
+                            elif((board[toindex-9] == 0 and board[toindex-4] == 1) or (board[toindex-7] == 0 and board[toindex-3] == 1)):
+                                sock2.sendall(("A").encode("utf-8"))
+                                goagain2 = 1
+                            else: sock2.sendall(("V").encode("utf-8"))
+                        else: sock2.sendall(("V").encode("utf-8"))
+                    elif(difference == -7): #DOWN AND RIGHT
+                        if(board[fromindex-3] == 1):
+                            board[fromindex-3] = 0
+                            board[fromindex] = 0
+                            board[toindex] = 2
+                        if(toindex>7):
+                            if(toindex%4 == 0):
+                                if(board[toindex-7] == 0 and board[toindex-3] == 1):
+                                    sock2.sendall(("A").encode("utf-8"))
+                                    goagain2 = 1
+                            elif(toindex%4 == 3):
+                                if(board[toindex-9] == 0 and board[toindex-4] == 1):
+                                    sock2.sendall(("A").encode("utf-8"))
+                                    goagain2 = 1
+                            elif((board[toindex-9] == 0 and board[toindex-4] == 2) or (board[toindex-7] == 0 and board[toindex-3] == 1)):
+                                sock2.sendall(("A").encode("utf-8"))
+                                goagain2 = 1
+                            else: sock2.sendall(("V").encode("utf-8"))
+                        else: sock2.sendall(("V").encode("utf-8"))
+                    else:
+                        print("Invalid move")
+                        sock2.sendall(("I").encode("utf-8"))
+                        goagain2=1
+                else:
+                    print("Please select a piece that is yours. Your pieces are on the bottom")
+                    sock2.sendall(("I").encode("utf-8"))
+                    goagain2=1
+            if(goagain2 == 0):
+                turn = 1
+
+        endgame = endgamecheck(board)
+        print(endgame)
+
+    if(endgame == 1):   
+        sock1.sendall(("W").encode("utf-8"))
+        sock2.sendall(("L").encode("utf-8"))
+    if(endgame == 2):   
+        sock1.sendall(("L").encode("utf-8"))
+        sock2.sendall(("W").encode("utf-8"))
+    
+
+def doRead(thesel, thesock, mask):
+    data = thesock.recv(1)
+    namethread = str(random.randint(1, 1000000))
+    global randomflag
+    global randomopponent
+    global waitlist
+    deleteflag = 0
+    tempptr = waitlist
+    followingptr = waitinguser()
+    if data:
+        print("recv:", data)
+
+    ##code for random room    
+        if(data.decode() == 'R'):
+            if(randomflag == 0):
+                randomopponent = thesock
+                randomflag = 1
+            else:
+                t1 = threading.Thread(name=namethread, target=Roomhandler, args=(randomopponent,thesock,mask))
+                t1.start()
+                thesel.unregister(randomopponent)
+                thesel.unregister(thesock)
+                randomflag = 0
+
+    ##code for specified room number
+        if(data.decode() == 'C'):
+            data = (thesock.recv(6)).decode()
+            print("Room number: ",data)
+            while(tempptr != 0):
+                if(tempptr.roomcode == data):
+                    t1 = threading.Thread(name=namethread, target=Roomhandler, args=(tempptr.socket,thesock,mask))
+                    t1.start()
+                    thesel.unregister(tempptr.socket)
+                    thesel.unregister(thesock)
+                    deleteflag = 1
+                    break
+                else:
+                    followingptr = tempptr
+                    tempptr = tempptr.nextuser
+            if(deleteflag == 1):
+                followingptr.nextuser = tempptr.nextuser
+                del tempptr
+                deleteflag = 0
+            else:
+                ##issue will be with this user being deleted at end of function
+                newuser = make_waitinguser(thesock,data)
+                followingptr.nextuser = newuser
+
+
+if __name__ == "__main__":
+
+    randomflag = 0
+    threadnum = 0
+    randomopponent = 0
+    
+    # create the selector
+    sel = selectors.DefaultSelector()
+    
+    # create the server socket
+    #  defaults family=AF_INET, type=SOCK_STREAM, proto=0, filno=None
+    serversoc = socket.socket()
+    
+    # make is non blocking
+    serversoc.setblocking(False)
+    
+    # bind to local host:5000
+    serversoc.bind(("localhost",50000))
+                   
+    # make passive with backlog=10
+    serversoc.listen(10)
+
+    waitlist = make_waitinguser(serversoc,0)
+    
+    # register the server socket
+    sel.register(serversoc, selectors.EVENT_READ, doAccept)
+    
+    # wait for incoming connections
+    while True:
+        
+        events = sel.select()
+        for key, mask in events:
+            callback = key.data
+            callback(sel, key.fileobj, mask)
+        
+
+    
+    # close the server socket
+    serversoc.close()
